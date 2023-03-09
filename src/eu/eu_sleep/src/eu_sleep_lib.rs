374ff99df78455972d6d08804f05ae036f8f5880
Ethan Raque <ethanraque@proton.me>
2023-03-08 23:49:49 -0500

    make eu_sleep functional

--- src/eu/eu_sleep/src/eu_sleep_lib.rs
+++ src/eu/eu_sleep/src/eu_sleep_lib.rs
@@ -1,8 +1,56 @@
 pub fn main(cli: Vec<String>) {
-    println!("{:?}", cli);
-    eu_sleep(std::time::Duration::new(5, 0));
+    eu_sleep(std::time::Duration::new(conv_to_secs(parse_input(cli)), 0));
 }
 
 fn eu_sleep(duration: std::time::Duration) {
     std::thread::sleep(duration);
+}
+
+fn parse_input(input: Vec<String>) -> Vec<(u64, char)> { // TODO adapt this to work with floats
+    let mut result: Vec<(u64, char)> = Vec::new();
+    for i in input {
+        let mut clean_input: String = i;
+        // Check if last character is a supported suffix or a number
+        // If it is supported remove that char from the string
+        let mut suffix: char = 's';
+        let ch = clean_input.chars().last().unwrap();
+        if ch == 's' || ch == 'm' || ch == 'h' || ch == 'd' {
+            suffix = ch;
+            clean_input.pop();
+        } else if ch.is_digit(10) != true {
+            println!("{} is not a supported suffix!", ch);
+            std::process::exit(1);
+        }
+
+        // Get the number
+        let mut value: String = String::new();
+        for c in clean_input.chars() {
+            if c.is_digit(10) {
+                value.push(c);
+            } else {
+                println!("Input contains non number values. Check that your suffix is in the right place!");
+                std::process::exit(1);
+            }
+        }
+        let value: u64 = value.parse().unwrap();
+        result.push((value, suffix));
+    }
+    result
+}
+
+fn conv_to_secs(parsed_input: Vec<(u64, char)>) -> u64 { // TODO adapt this to work with floats
+    let mut running: u64 = 0;
+    for (n, c) in parsed_input {
+        running += match c {
+            's' => n,
+            'm' => n * 60,
+            'h' => n * 3600,
+            'd' => n * 86400,
+            _ => {
+                println!("Somehow your parsed input contains an invalid character. This should literally be impossible...");
+                std::process::exit(2);
+            }
+        };
+    }
+    running
 }
---
+++ todo.md
@@ -0,0 +1,6 @@
+
+#eu_sleep
+  (A) Fix eu_sleep to work with floats {c}
+    - Write function to convert a float number of seconds to seconds and nanoseconds
+    - Adjust functions to work with floats
+  (B) Write tests for eu_sleep
